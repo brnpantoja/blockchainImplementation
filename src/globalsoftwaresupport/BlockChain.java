1d0dc71173180e6e7c39cbfcc93980a29a09a9b9
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-09-16 19:59:14 -0300

    Class BlockChain

--- src/globalsoftwaresupport/BlockChain.java
+++ src/globalsoftwaresupport/BlockChain.java
@@ -1,4 +1,37 @@
 package globalsoftwaresupport;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BlockChain {
+
+    // razão imutável
+    // não pode remover blocos
+    private List<Block> blockChain;
+
+    public BlockChain() {
+        this.blockChain = new LinkedList<>();
+    }
+
+    public void addBlock(Block block) {
+        this.blockChain.add(block);
+    }
+
+    public List<Block> getBlockChain() {
+        return this.blockChain;
+    }
+
+    public int getSize() {
+        return this.blockChain.size();
+    }
+
+    @Override
+    public String toString() {
+
+        String s = "";
+        for (Block block : this.blockChain)
+            s += block.toString() + "\n";
+
+        return s;
+    }
 }
--- src/globalsoftwaresupport/SHA256Helper.java
+++ src/globalsoftwaresupport/SHA256Helper.java
@@ -11,7 +11,7 @@ public class SHA256Helper {
             byte[] hash = digest.digest(data.getBytes("UTF-8"));
 
             // acabar com os valores hexadecimais e não bytes
-            StringBuffer hexadecimalString = new StringBuffer();
+            StringBuilder hexadecimalString = new StringBuilder();
             for (int i = 0; i < hash.length; i++) {
                 String hexadecimal = Integer.toHexString(0xff & hash[i]);
                 // preenchimento
